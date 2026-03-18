627f80cb8ff022ce21cb440d587259444cb7dedb
OLC <utsumabo@ritsukage.com>
2026-03-18 09:13:39 +0800

    ✨ feat: 移动 MineMaterialVar 类到新的文件，优化代码结构

---
+++ Cards/DynamicVars/MineMaterialVar.cs
@@ -0,0 +1,52 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using STS2_WineFox.Cards.Basic;
+using STS2_WineFox.Powers;
+
+namespace STS2_WineFox.Cards.DynamicVars
+{
+    public sealed class MineMaterialVar<TPower>(string name, decimal baseValue)
+        : DynamicVar(name, baseValue)
+        where TPower : PowerModel
+    {
+        public override void UpdateCardPreview(CardModel card, CardPreviewMode previewMode, Creature? target,
+            bool runGlobalHooks)
+        {
+            PreviewValue = card is BasicMine mine
+                ? CalculateAmount(mine, runGlobalHooks)
+                : BaseValue;
+        }
+
+        protected override decimal GetBaseValueForIConvertible()
+        {
+            return _owner is not BasicMine mine
+                ? base.GetBaseValueForIConvertible()
+                : CalculateAmount(mine, CombatManager.Instance.IsInProgress);
+        }
+
+        private decimal CalculateAmount(BasicMine card, bool runGlobalHooks)
+        {
+            if (card.Owner?.Creature == null)
+                return BaseValue;
+
+            var amount = BaseValue +
+                         (card.Owner.Creature.Powers.OfType<DiggingPower>().FirstOrDefault()?.Amount ?? 0m);
+
+            if (!runGlobalHooks || card.CombatState == null)
+                return amount;
+
+            return Hook.ModifyPowerAmountGiven(
+                card.CombatState,
+                ModelDb.Power<TPower>(),
+                card.Owner.Creature,
+                amount,
+                card.Owner.Creature,
+                card,
+                out _);
+        }
+    }
+}
