6ed1066578874241d86f220e79073e9c81ab3835
LuoTianOrange <2977277015@qq.com>
2026-04-15 23:11:24 +0800

    feat: 调整[唱片机]效果为：抽1张牌。消耗1钻石来获得2层仪式。

--- Cards/Uncommon/RecordPlayer.cs
+++ Cards/Uncommon/RecordPlayer.cs
@@ -1,6 +1,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 using STS2_WineFox.Commands;
@@ -13,23 +14,35 @@ namespace STS2_WineFox.Cards.Uncommon
         1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
     {
         protected override IEnumerable<DynamicVar> CanonicalVars =>
-            [new("RitualPower", 2m)];
+            [new CardsVar(1), new("RitualPower", 2m)];
 
         public override CardAssetProfile AssetProfile => Art(Const.Paths.CardRecordPlayer);
 
-        protected override bool IsPlayable =>
-            Owner.Creature.Powers.OfType<DiamondPower>().Any(p => (decimal)p.Amount >= 1);
+        protected override bool IsPlayable
+        {
+            get
+            {
+                var creature = Owner?.Creature;
+                if (creature == null) return false;
+                return creature.Powers.OfType<DiamondPower>().Any(p => (decimal)p.Amount >= 1);
+            }
+        }
 
+        protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+            [HoverTipFactory.FromPower<RitualPower>()];
+        
         protected override async Task OnPlay(
             PlayerChoiceContext choiceContext,
             CardPlay play)
         {
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+
             if (play.IsFirstInSeries && !MaterialCmd.IsFreePlay(play))
             {
-                var diamondPower = Owner.Creature.Powers
+                var hasDiamond = Owner.Creature.Powers
                     .OfType<DiamondPower>()
-                    .FirstOrDefault(p => (decimal)p.Amount >= 1);
-                if (diamondPower == null) return;
+                    .Any(p => (decimal)p.Amount >= 1);
+                if (!hasDiamond) return;
             }
 
             await MaterialCmd.LoseMaterial<DiamondPower>(this, 1m, play);
